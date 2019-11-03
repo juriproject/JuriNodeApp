a332ea79392ab772fe014451278a721c11fca22f
Markus Waas <mail@markuswaas.de>
2019-11-04 12:05:18 +1300

    Add 'REVERT' to parseRevertMessage

--- helpers/parseRevertMessage.js
+++ helpers/parseRevertMessage.js
@@ -2,13 +2,20 @@ const parseRevertMessage = error => {
   try {
     if (error.indexOf('reverted by the EVM') === -1)
       // local blockchain
-      return error.slice(
-        error.indexOf('VM Exception while processing transaction: revert') + 50
+      return (
+        'REVERT: ' +
+        error.slice(
+          error.indexOf('VM Exception while processing transaction: revert') +
+            50
+        )
       )
 
     // SKALE chain
-    return JSON.parse(error.slice(error.indexOf('reverted by the EVM') + 20))
-      .revertReason
+    return (
+      'REVERT: ' +
+      JSON.parse(error.slice(error.indexOf('reverted by the EVM') + 20))
+        .revertReason
+    )
   } catch (error) {
     console.log({ error })
 
