b86f1874366db347ef653f33c273175874b41ab6
Markus Waas <mail@markuswaas.de>
2019-11-26 09:43:23 +1300

    update dependencies

--- management/scripts/updateJuriNodeAppForAwsNodes.sh
+++ management/scripts/updateJuriNodeAppForAwsNodes.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-export $(egrep -v '^#' awsDnsNames.env | xargs)
+export $(egrep -v '^#' ../../config/awsDnsNames.env | xargs)
 
 CONTROLLER_PUBLIC_KEY=$1
 CONTROLLER_PRIVATE_KEY=$2
 
 echo 'Updating repos...'
-ssh -o StrictHostKeyChecking=no -i "../JuriNodes.pem" "ubuntu@${CONTROLLER_NODE}" "cd JuriNodeApp && git pull"
-#ssh -o StrictHostKeyChecking=no -i "../JuriNodes.pem" "ubuntu@${CONTROLLER_NODE}" "cd JuriNodeApp && npm i"
+ssh -o StrictHostKeyChecking=no -i "../../JuriNodes.pem" "ubuntu@${CONTROLLER_NODE}" "cd JuriNodeApp && git pull"
+#ssh -o StrictHostKeyChecking=no -i "../../JuriNodes.pem" "ubuntu@${CONTROLLER_NODE}" "cd JuriNodeApp && npm i"
 
 for ((index=1; index-1<5; ++index)); do
     HOST="NODE$index"
-    ssh -o StrictHostKeyChecking=no -i "../JuriNodes.pem" "ubuntu@${!HOST}" "cd JuriNodeApp && git pull"
-    # ssh -o StrictHostKeyChecking=no -i "../JuriNodes.pem" "ubuntu@${!HOST}" "cd JuriNodeApp && npm i"
+    ssh -o StrictHostKeyChecking=no -i "../../JuriNodes.pem" "ubuntu@${!HOST}" "cd JuriNodeApp && git pull"
+    # ssh -o StrictHostKeyChecking=no -i "../../JuriNodes.pem" "ubuntu@${!HOST}" "cd JuriNodeApp && npm i"
 done
 echo 'Updating repos finished!'
